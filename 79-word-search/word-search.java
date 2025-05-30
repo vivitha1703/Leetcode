69fbaf92996a4ecf69ba89c7e9b474887c3c6179
Vivitha <139707101+vivitha1703@users.noreply.github.com>
2025-05-30 09:28:34 +0530

    Time: 154 ms (43.88%) | Memory: 41.5 MB (78.03%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,36 +1,35 @@
 class Solution {
-    public boolean search(char[][] board, char[] word, int index, int i, int j){
-        if(index>=word.length){
-            //System.out.println("index:"+index);
-            return true;
-        }
-        if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]!=word[index] || board[i][j]=='@'){
-            return false;
-        }
-        char temp = board[i][j];
-        //System.out.println("letter:"+temp+" i:"+i+" j:"+j+" index:"+index);
-        board[i][j] = '@';
-        boolean ans = (search(board,word,index+1,i+1,j)) ||
-                      (search(board,word,index+1,i-1,j)) ||
-                      (search(board,word,index+1,i,j+1)) ||
-                      (search(board,word,index+1,i,j-1));
-        board[i][j] = temp; 
-        return ans;
-    }
-    public boolean exist(char[][] board, String wordStr) {
-        char[] word = wordStr.toCharArray();
-        int row = board.length;
-        int col = board[0].length;
-        int index=0;
-        boolean result = false;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(board[i][j]==word[index]){
-                    result = search(board,word,index,i,j);
-                    if(result==true) return result;
+    public boolean exist(char[][] board, String word) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(search(board,word,i,j,0,visited)){
+                    return true;
                 }
             }
         }
-        return result;
+    return false;    
+    }
+    public boolean search(char[][] board, String word, int row, int col, int index,  boolean[][] visited){
+        if(index==word.length()){
+            return true;
+        }
+        if(row<0 || col<0 || row>=board.length || col>=board[0].length || board[row][col]!=word.charAt(index) || visited[row][col]){
+            return false;
+        }
+        
+        if(board[row][col]==word.charAt(index)){
+            visited[row][col]=true;
+            boolean left = search(board,word,row,col-1,index+1,visited);
+            boolean right = search(board,word,row,col+1,index+1,visited);
+            boolean top = search(board,word,row-1,col,index+1,visited);
+            boolean bottom = search(board,word,row+1,col,index+1,visited);
+            
+            if(left || right || top || bottom){
+                return true;
+            }
+            visited[row][col] = false;
+        }
+        return false;
     }
 }
