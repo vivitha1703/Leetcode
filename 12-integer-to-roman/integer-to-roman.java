13238a72ef9fdde4a9b8f89d59d71dd5b77f4e77
Vivitha <139707101+vivitha1703@users.noreply.github.com>
2025-07-10 09:03:17 +0530

    Time: 93 ms (6.28%) | Memory: 45 MB (17.69%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = new int[]{1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        String[] romans = new String[]{"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        String result="";
+        while(num!=0){
+            for(int i=values.length-1;i>=0;i--){
+                if(num>=values[i]){
+                    result += romans[i];
+                    num = num - values[i];
+                    System.out.println(result);
+                    System.out.println(num);
+                    break;
+                }
+            }
+        } 
+        return result;
+    }
+}
