efe93fa4f97907618e4d30bad28839e4912a267c
Vivitha <139707101+vivitha1703@users.noreply.github.com>
2025-07-25 22:28:23 +0530

    Time: 3 ms (82.55%) | Memory: 44.8 MB (92.86%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,29 +1,28 @@
 class Solution {
+    public boolean isVowel(char c){
+        c = Character.toLowerCase(c);
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+            return true;
+        }
+        return false;
+    }
     public String reverseVowels(String s) {
         StringBuilder sb = new StringBuilder(s);
-        int start = 0, end = s.length() - 1;
-
-        while (start < end) {
-            while (start < end && !isVowel(sb.charAt(start))) {
+        int start=0,end=s.length()-1;
+        int stChar,endChar;
+        while(start<end){
+            while(start<end && !isVowel(sb.charAt(start))){
                 start++;
             }
-            while (start < end && !isVowel(sb.charAt(end))) {
+            while(start<end && !isVowel(sb.charAt(end))){
                 end--;
             }
-
             char temp = sb.charAt(start);
-            sb.setCharAt(start, sb.charAt(end));
-            sb.setCharAt(end, temp);
-
+            sb.setCharAt(start,sb.charAt(end));
+            sb.setCharAt(end,temp);
             start++;
             end--;
         }
-
         return sb.toString();
-    }
-
-    private boolean isVowel(char ch) {
-        ch = Character.toLowerCase(ch);
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
 }
