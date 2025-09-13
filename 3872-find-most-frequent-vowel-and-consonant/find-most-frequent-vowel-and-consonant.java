34ea31557962a73df79ff4e9f8a8651bd8f26dfa
Vivitha <139707101+vivitha1703@users.noreply.github.com>
2025-09-13 13:55:26 +0530

    Time: 17 ms (5.69%) | Memory: 43.7 MB (6.38%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        HashMap<Character,Integer> vowel_map = new HashMap<>();
+        HashMap<Character,Integer> cons_map = new HashMap<>();
+        int vmax=0, cmax=0;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                vowel_map.put(c,vowel_map.getOrDefault(c,0)+1);
+            }
+            else {
+                cons_map.put(c,cons_map.getOrDefault(c,0)+1);
+            }
+        }
+
+        System.out.println(vowel_map);
+        System.out.println(cons_map);
+
+        for(Map.Entry<Character,Integer> entry:vowel_map.entrySet()){
+            if(entry.getValue()>vmax){
+                vmax = entry.getValue();
+            }
+        }
+        for(Map.Entry<Character,Integer> entry:cons_map.entrySet()){
+            if(entry.getValue()>cmax){
+                cmax = entry.getValue();
+            }
+        }
+        
+        System.out.println(vmax);
+        System.out.println(cmax);
+        return vmax+cmax;
+        
+    }
+}
