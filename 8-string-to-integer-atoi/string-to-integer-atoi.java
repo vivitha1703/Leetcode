2317ef5cc74146b394999474a186496e93c22e60
Vivitha <139707101+vivitha1703@users.noreply.github.com>
2025-10-23 14:06:42 +0530

    Time: 1 ms (100.00%) | Memory: 42.2 MB (96.52%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int myAtoi(String s) {
+        long result=0;
+        int index=0;
+        boolean negative=false;
+        s = s.trim();
+        if(s.length()==0) return 0;
+        char[] strArr = s.toCharArray();
+        if(strArr[index] == '-' || strArr[index]=='+'){
+            if(strArr[index] == '-')negative = true;
+            index++;
+        }
+        for(int i=index;i<strArr.length;i++){
+            if(!Character.isDigit(strArr[i])){
+                break;
+            }
+            else{
+                result = result*10+(Character.getNumericValue(strArr[i]));
+            }
+            if(!negative && result>Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }
+            if(negative && -result<Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
+            
+        }
+        if(negative) result = -result;
+        return (int)result;
+    }
+}
