d8c997bfb5dd3b590c8b2e53325ec06df0ac8264
Vivitha <139707101+vivitha1703@users.noreply.github.com>
2025-10-24 11:04:21 +0530

    Time: 4 ms (77.08%) | Memory: 44.9 MB (32.13%) - LeetSync

--- 423-reconstruct-original-digits-from-english/reconstruct-original-digits-from-english.java
+++ 423-reconstruct-original-digits-from-english/reconstruct-original-digits-from-english.java
@@ -1,29 +1,26 @@
 class Solution {
     public String originalDigits(String s) {
-        int[] count = new int[26];
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
-        }
-
-        int[] digit = new int[10];
-        digit[0] = count['z' - 'a']; // zero
-        digit[2] = count['w' - 'a']; // two
-        digit[4] = count['u' - 'a']; // four
-        digit[6] = count['x' - 'a']; // six
-        digit[8] = count['g' - 'a']; // eight
-        digit[3] = count['h' - 'a'] - digit[8];
-        digit[5] = count['f' - 'a'] - digit[4];
-        digit[7] = count['s' - 'a'] - digit[6];
-        digit[1] = count['o' - 'a'] - digit[0] - digit[2] - digit[4];
-        digit[9] = count['i' - 'a'] - digit[5] - digit[6] - digit[8];
-
+        int freq[] = new int[26];
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i <= 9; i++) {
-            while (digit[i]-- > 0) {
+        for(char c:s.toCharArray()){
+            freq[c-'a']++;
+        }    
+        int digit[] = new int[10];
+        digit[0] = freq['z'-'a'];
+        digit[2] = freq['w'-'a'];
+        digit[4] = freq['u'-'a'];
+        digit[6] = freq['x'-'a'];
+        digit[8] = freq['g'-'a'];
+        digit[3] = freq['h'-'a']-digit[8];
+        digit[5] = freq['f'-'a']-digit[4];
+        digit[7] = freq['s'-'a']-digit[6];
+        digit[1] = freq['o'-'a']-digit[0]-digit[2]-digit[4];
+        digit[9] = freq['i'-'a']-digit[5]-digit[6]-digit[8];
+        for(int i=0;i<10;i++){
+            while(digit[i]-->0){
                 result.append(i);
             }
         }
         return result.toString();
-        
     }
 }
