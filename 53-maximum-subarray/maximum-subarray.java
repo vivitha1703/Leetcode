3c68447cd89dad6c765ad769bfecb4c932e12640
Vivitha <139707101+vivitha1703@users.noreply.github.com>
2026-07-17 13:05:42 +0530

    Time: 1 ms (99.97%) | Memory: 77.2 MB (48.58%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,14 +3,8 @@ class Solution {
         int currSum = nums[0];
         int maxSum = nums[0];
         for(int i=1;i<nums.length;i++){
-            if(nums[i]+currSum > nums[i]){
-                currSum = nums[i]+currSum;
-            } else {
-                currSum = nums[i];
-            }
-            if(currSum > maxSum){
-                maxSum = currSum;
-            }
+            currSum = Math.max(currSum+nums[i], nums[i]);
+            maxSum = Math.max(currSum,maxSum);
         }
         return maxSum;
     }
